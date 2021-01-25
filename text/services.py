d250a396a59891e6e34ece5e582eabf2538aa81c
yunis <yhuseynov19@gmail.com>
2021-01-25 20:05:13 +0300

    add async

--- text/services.py
+++ text/services.py
@@ -1,13 +1,12 @@
-from sqlalchemy_searchable import search
-from sqlalchemy.orm import Session
 import csv
 from core.db import database
-from sqlalchemy import select
 from datetime import datetime
 from io import TextIOWrapper
+from sqlalchemy_searchable import search
+from sqlalchemy.orm import Session
 from typing import List
 
-from .models import Text,texts
+from .models import Text, texts
 from .schemas import TextCreate
 
 
@@ -23,25 +22,10 @@ async def create_text(item: TextCreate):
     return {**item.dict(), "id": create_query_fetch_and_id}
 
 
-
 def serch_text(search_text: str, db: Session):
-    test = texts.select()
-    print(test.__dict__)
-    # print(test)
-    print()
     query = db.query(Text)
-    for i in query._entities:
-        print(i.__dict__)
-        print()
-        print(type(i))
-        print()
-
-    # print(query)
-    print()
     query = search(query, search_text).limit(20)
-    print(query)
     get_query = query.all()
-
     return get_query
 
 
@@ -54,7 +38,7 @@ async def import_data_fromcsv_topostgres(buffer: TextIOWrapper) -> List:
         text = line[0]
         # invert str to date, because asyncpg can't convert str to datetime, but if we use standart sqlalchemy , we
         # can pass str
-        date = datetime.strptime(line[1],'%Y-%m-%d %H:%M:%S')
+        date = datetime.strptime(line[1], '%Y-%m-%d %H:%M:%S')
         rubricks = line[2]
         create_line_db_query = texts.insert().values(text=text, date=date, rubricks=rubricks)
         create_line_db_query_fetch_and_id = await database.execute(create_line_db_query)
@@ -62,12 +46,20 @@ async def import_data_fromcsv_topostgres(buffer: TextIOWrapper) -> List:
 
     return created_text_id
 
+
 async def delete_text(text_id: int):
     find_query = texts.select().where(texts.c.id == text_id)
-    find_query_fetch = await database.fetch_one(query = find_query)
+    find_query_fetch = await database.fetch_one(query=find_query)
     print(find_query_fetch)
     if find_query_fetch is None:
         return None
     delete_query = texts.delete().where(texts.c.id == text_id)
     delete_query_fetch = await database.execute(delete_query)
+    return {**find_query_fetch}
+
+async def find_text(text_id: int):
+    find_query = texts.select().where(texts.c.id == text_id)
+    find_query_fetch = await database.fetch_one(query=find_query)
+    if find_query_fetch is None:
+        return None
     return {**find_query_fetch}
--- text/text.py
+++ text/text.py
@@ -1,35 +1,39 @@
-import shutil
-from typing import List
 from fastapi import APIRouter, Depends, UploadFile, File
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
-from fastapi import  HTTPException
+from typing import List
 
 from core.utils import get_db
-from .services import get_text_list, create_text, serch_text, import_data_fromcsv_topostgres,delete_text
+from .services import get_text_list, create_text, serch_text, import_data_fromcsv_topostgres, delete_text, find_text
 from .schemas import TextBase, TextList, TextCreate, TextDetail
-
-
 
 router = APIRouter()
 
+
 # changed
-@router.get("/", response_model = List[TextList])
+@router.get("/", response_model=List[TextList])
 async def get_text_list_request():
+    """Get all texts"""
     return await get_text_list()
+
 
 # changed
 @router.post("/", status_code=201)
 async def create_text_request(item: TextCreate):
+    """Create text"""
     return await create_text(item)
 
 
-@router.post("/serch/{search_text}", response_model = List[TextList])
+@router.post("/serch/{search_text}", response_model=List[TextList])
 def serch_text_request(search_text: str, db: Session = Depends(get_db)):
+    """Serch text"""
     return serch_text(search_text, db)
 
-# changed
-@router.post("/csv", status_code = 201)
+
+
+@router.post("/csv", status_code=201)
 async def upload_image(file: UploadFile = File(...)):
+    """Import csv file to db with 3 rows"""
     try:
         with open(f'{file.filename}', "r") as buffer:
             created_texts_id = await  import_data_fromcsv_topostgres(buffer)
@@ -39,8 +43,17 @@ async def upload_image(file: UploadFile = File(...)):
 
 
 @router.delete("/delete/{text_id}", response_model=TextDetail)
-async def delete_text_request(text_id:int):
+async def delete_text_request(text_id: int):
+    """Delete with id"""
     deleted_text = await delete_text(text_id)
     if deleted_text is None:
         raise HTTPException(status_code=418, detail="couldn't find text")
     return deleted_text
+
+@router.get("/{text_id}", response_model=TextDetail)
+async def get_text(text_id: int):
+    """Find text with id"""
+    finded_text = await find_text(text_id)
+    if finded_text is None:
+        raise HTTPException(status_code=418, detail="couldn't find text")
+    return finded_text
